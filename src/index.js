5af388f9c7a4291c0192cf3a436337f870049d80
Hong Jeesoo <jeesoo.hong@gmail.com>
2018-11-08 17:35:11 +0900

    옵션 선택 기능, 장바구니 기능 추가

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.API_URL
-});
+})
 
 api.interceptors.request.use(function (config) {
   const token = localStorage.getItem('token')
@@ -16,12 +16,13 @@ api.interceptors.request.use(function (config) {
 });
 
 const templates = {
+  layout: document.querySelector('#layout').content,
   loginForm: document.querySelector("#login-form").content,
   productList: document.querySelector("#product-list").content,
   productItem: document.querySelector("#product-item").content,
-  // productForm: document.querySelector("#product-form").content,
   productDetail: document.querySelector("#product-detail").content,
-  // commentItem: document.querySelector("#comment-item").content
+  cartList: document.querySelector('#cart-list').content,
+  cartItem: document.querySelector('#cart-item').content
 };
 
 const rootEl = document.querySelector(".root");
@@ -34,52 +35,72 @@ const rootEl = document.querySelector(".root");
 // 5. 이벤트 리스너 등록하기
 // 6. 템플릿을 문서에 삽입
 
-// 메인 페이지
-// 1. 템플릿 복사
-// 2. 요소 선택
+function drawFragment(frag) {
+  // 1. 템플릿 복사
+  const layoutFrag = document.importNode(templates.layout, true);
+  // 2. 요소 선택
+  const signInEl = layoutFrag.querySelector('.sign-in')
+  const signOutEl = layoutFrag.querySelector('.sign-out')
+  const myCartEl = layoutFrag.querySelector('.my-cart')
+  const entireItemEl = layoutFrag.querySelector('.entire')
+  const flowerEl = layoutFrag.querySelector('.flower')
+  const plantEl = layoutFrag.querySelector('.plant')
+  const cactusEl = layoutFrag.querySelector('.cactus')
+  const hangingEl = layoutFrag.querySelector('.hanging')
+  const orchidEl = layoutFrag.querySelector('.orchid')
+  const mainEl = layoutFrag.querySelector(".main");
+  // 3. 필요한 데이터 불러오기
+  if(localStorage.token){
+    signOutEl.classList.remove('hidden')
+    signInEl.classList.add('hidden')
+  }
+  // 4. 내용 채우기
+  // 5. 이벤트 리스너 등록하기
+  signInEl.addEventListener('click', async e => {
+    e.preventDefault()
+    await drawLoginForm()
+  })
+  signOutEl.addEventListener('click', async e => {
+    e.preventDefault()
+    localStorage.removeItem("token");
+    alert('로그아웃 되었습니다.')
+    await drawProductList()
+  })
+  myCartEl.addEventListener('click', async e => {
+    const userId = localStorage.token
+    e.preventDefault()
+    await drawCartList(userId)
+  })
+  entireItemEl.addEventListener('click', async e => {
+    e.preventDefault()
+    await drawProductList()
+  })
+  flowerEl.addEventListener("click", async e => {
+    e.preventDefault();
+    await drawProductList('flower');
+  });
+  plantEl.addEventListener("click", async e => {
+    e.preventDefault();
+    await drawProductList('plant');
+  });
+  cactusEl.addEventListener("click", async e => {
+    e.preventDefault();
+    await drawProductList('cactus');
+  });
+  hangingEl.addEventListener("click", async e => {
+    e.preventDefault();
+    await drawProductList('hanging');
+  });
+  orchidEl.addEventListener("click", async e => {
+    e.preventDefault();
+    await drawProductList('orchid');
+  });
+  // 6. 템플릿을 문서에 삽입
+  mainEl.appendChild(frag);
 
-// member 영역
-const signInEl = document.querySelector('.sign-in')
-
-// category 영역
-const entireItemEl = document.querySelector('.entire')
-const flowerEl = document.querySelector('.flower')
-const plantEl = document.querySelector('.plant')
-const cactusEl = document.querySelector('.cactus')
-const hangingEl = document.querySelector('.hanging')
-const orchidEl = document.querySelector('.orchid')
-// 3. 필요한 데이터 불러오기
-// 4. 내용 채우기
-// 5. 이벤트 리스너 등록하기
-signInEl.addEventListener('click', async e => {
-  e.preventDefault()
-  await drawLoginForm()
-})
-entireItemEl.addEventListener('click', async e => {
-  e.preventDefault()
-  await drawProductList()
-})
-flowerEl.addEventListener("click", async e => {
-  e.preventDefault();
-  await drawProductList('flower');
-});
-plantEl.addEventListener("click", async e => {
-  e.preventDefault();
-  await drawProductList('plant');
-});
-cactusEl.addEventListener("click", async e => {
-  e.preventDefault();
-  await drawProductList('cactus');
-});
-hangingEl.addEventListener("click", async e => {
-  e.preventDefault();
-  await drawProductList('hanging');
-});
-orchidEl.addEventListener("click", async e => {
-  e.preventDefault();
-  await drawProductList('orchid');
-});
-// 6. 템플릿을 문서에 삽입
+  rootEl.textContent = ''
+  rootEl.appendChild(layoutFrag)
+}
 
 async function drawLoginForm() {
   // 1. 템플릿 복사
@@ -101,14 +122,14 @@ async function drawLoginForm() {
       password
     })
     localStorage.setItem('token', res.data.token)
+    drawProductList()
   })
 
   // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = ''
-  rootEl.appendChild(frag)
+  drawFragment(frag);
 }
 
-async function drawProductList(category = undefined) {
+async function drawProductList(categoryDetail = undefined) {
   // 1. 템플릿 복사
   const frag = document.importNode(templates.productList, true);
 
@@ -117,12 +138,12 @@ async function drawProductList(category = undefined) {
   const listEl = frag.querySelector(".product-list");
 
   // 3. 필요한 데이터 불러오기
-  const { data: prevProductList } = await api.get("/products", {
+  const { data: productList } = await api.get("/products", {
     params: {
+      category: categoryDetail,
       _embed: "options"
     }
   })
-  const productList = prevProductList.filter(item => item.category === category || category === undefined);
 
   // 4. 내용 채우기
     numberEl.textContent = productList.length
@@ -153,17 +174,18 @@ async function drawProductList(category = undefined) {
 
   // 5. 이벤트 리스너 등록하기
   // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = "";
-  rootEl.appendChild(frag);
+  drawFragment(frag);
 }
 
 async function drawProductDetail(productId) {
   // 1. 템플릿 복사
   const frag = document.importNode(templates.productDetail, true)
-
   // 2. 요소 선택
   const categoryRouteEl = frag.querySelector('.third')
   const titleRouteEl = frag.querySelector('.fourth')
+
+  const formEl = frag.querySelector('.cart-form')
+  const selectEl = frag.querySelector('.select-option')
   const titleEl = frag.querySelector('.title')
   const descEl = frag.querySelector('.description')
   const priceEl = frag.querySelector('.price')
@@ -181,15 +203,82 @@ async function drawProductDetail(productId) {
   // 4. 내용 채우기
   categoryRouteEl.textContent = category
   titleRouteEl.textContent = title
+
+  options.forEach(option => {
+    const optionEl = document.createElement("option")
+    optionEl.setAttribute('value', option.id)
+    optionEl.textContent = option.title
+    selectEl.appendChild(optionEl)
+  })
+
   titleEl.textContent = title
   descEl.textContent = description
   priceEl.textContent = options[0].price
   mainImgEl.setAttribute("src", mainImgUrl)
   detailImgEl.setAttribute("src", detailImgUrls[0])
   // 5. 이벤트 리스너 등록하기
+  formEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const optionId = parseInt(e.target.elements.option.value)
+    const quantity = parseInt(e.target.elements.quantity.value)
+    await api.post('/cartItems', {
+      optionId,
+      quantity
+    })
+    alert('장바구니에 담겼습니다.')
+  })
   // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = ''
-  rootEl.appendChild(frag)
+  drawFragment(frag);
+}
+
+async function drawCartList() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.cartList, true)
+  // 2. 요소 선택
+  const cartListEl = frag.querySelector('.cart-list')
+  const cartTotalPriceEl = frag.querySelector('.cart-total-price')
+  const cartOrderBtnEl = frag.querySelector('.cart-total')
+  // 3. 필요한 데이터 불러오기
+  const { data: cartList } = await api.get('/cartItems', {
+    params: {
+      _embed: 'options'
+    }
+  })
+  const { data: optionList } = await api.get('/options/', {
+    params: {
+      _expand: 'product'
+    }
+  })
+  // 4. 내용 채우기
+  cartList.forEach(cartItem => {
+    // 1. 템플릿 복사
+    const frag = document.importNode(templates.cartItem, true)
+    // 2. 요소 선택
+    const mainImgEl = frag.querySelector('.cart-main-img')
+    const titleEl = frag.querySelector('.title')
+    const priceEl = frag.querySelector('.price')
+    const quantityEl = frag.querySelector('.quantity')
+    const cartDeleteBtnEl = frag.querySelector('.cart-delete')
+    // 3. 필요한 데이터 불러오기
+    quantityEl.textContent = cartItem.quantityEl
+    const option = optionList.find(item => item.id === cartItem.optionId)
+    priceEl.textContent = option.price
+    mainImgEl.setAttribute("src", option.product.mainImgUrl)
+    titleEl.textContent = option.product.title
+    // 4. 내용 채우기
+    // 5. 이벤트 리스너 등록하기
+    cartDeleteBtnEl.addEventListener('click', async e => {
+      e.preventDefault()
+      await api.delete('/cartItems/' + cartItem.id)
+      alert(`선택한 항목이 장바구니에서 삭제 되었습니다.`);
+      drawCartList()
+    })
+    // 6. 템플릿을 문서에 삽입
+    cartListEl.appendChild(frag)
+  })
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
+  drawFragment(frag);
 }
 
 drawProductList()
